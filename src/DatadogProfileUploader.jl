5ff08e37bfaebc90b01100d58085d6e8e78c83bb
Pete Vilter <pete.vilter@gmail.com>
2022-01-28 11:07:52 -0500

    initial attempt at the uploader

--- src/DatadogProfileUploader.jl
+++ src/DatadogProfileUploader.jl
@@ -1,5 +1,35 @@
 module DatadogProfileUploader
 
-greet() = print("Hello World!")
+import HTTP
+
+struct DDConfig
+    url::String
+    api_key::String
+end
+
+struct Profile
+    start::Datetime
+    finish::Datetime
+    proto_path::String
+end
+
+function upload(config::DDConfig, profile::Profile)
+    # do HTTP request
+    headers = [
+        "DD-API-KEY" => config.api_key,
+    ]
+    body = HTTP.Form([
+        "version" => "3",
+        "start" => format(profile.start, RFC3339),
+        "end" => format(profile.finish, RFC3339),
+        # tags[]
+        "data[my_prof]" => HTTP.Multipart(
+            "pprof-data",
+            open(Profile.proto_path), # proto
+            "application/octet-stream"
+        ),
+    ])
+    HTTP.post(config.url, headers, body)
+end
 
 end # module
