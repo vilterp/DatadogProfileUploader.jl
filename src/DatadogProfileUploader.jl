b092fe3d451f5117dd309aa31c5c65dd41948abb
Pete Vilter <pete.vilter@gmail.com>
2022-10-20 18:50:56 -0400

    go back to taking profile

--- src/DatadogProfileUploader.jl
+++ src/DatadogProfileUploader.jl
@@ -21,29 +21,20 @@ struct SerializedProfile
 end
 
 function profile_and_upload(config, f)
-    #Profile.clear()
+    Profile.clear()
     start = now()
-    println("taking profile")
-    # Profile.@profile f()
-    println("done taking profile")
+    Profile.@profile f()
     finish = now()
 
-    path = "profile.pb.gz" # TODO: temp file?
-    #pprof(; web=false, out=path)
-    # upload(config, SerializedProfile(start, finish, "cpu", path))
-    upload(config, SerializedProfile(start, finish, "cpu", "../testdata/cpu.pprof.gz"))
+    pprof(; web=false, out=path)
+    upload(config, SerializedProfile(start, finish, "cpu", path))
 end
 
 const ExpectedDateFormat = DateFormat("yyyy-mm-dd\\THH:MM:SSZ")
 
 function upload(config::DDConfig, profile::SerializedProfile)
-    headers = [
-        "User-Agent" => "Go-http-client/1.1",
-        "DD-API-KEY" => config.api_key,
-    ]
-    
     # do HTTP request
-    name = "$(profile.type).pprof"
+    profile_name = "$(profile.type).pprof"
     parts = Pair{String,Any}[
         "version" => "3",
         "family" => "go",
@@ -60,25 +51,20 @@ function upload(config::DDConfig, profile::SerializedProfile)
         "tags[]" => "runtime_os:darwin",
         "tags[]" => "runtime-id:d9b59eb6-d5f7-4534-ae82-793f29397",
         "tags[]" => "version:1.0",
-        "tags[]" => "service:go-sorter",
+        "tags[]" => "service:julia-sorter",
         "tags[]" => "env:example",
         
-        # other stuff
-        "data[metrics.json]" => HTTP.Multipart(
-            "pprof-data",
-            IOBuffer("""[["go_alloc_bytes_per_sec",637031112.2133334],["go_allocs_per_sec",69.76],["go_frees_per_sec",37.10666666666667],["go_heap_growth_bytes_per_sec",9128779.946666667],["go_gcs_per_sec",1.7066666666666668],["go_gc_pause_time",0.0002923213237688478],["go_max_gc_pause_time",8196327]]"""),
-            "application/octet-stream",
-        ),
-        "data[$(name)]" => HTTP.Multipart(
+        "data[$profile_name]" => HTTP.Multipart(
             "pprof-data",
             open(profile.proto_path), # proto
             "application/octet-stream"
         ),
     ]
     body = HTTP.Form(parts)
-    url = "https://intake.profile.datadoghq.com/v1/input"
+    # url = "https://intake.profile.datadoghq.com/v1/input"
+    url = "http://localhost:8126/profiling/v1/input"
     println("posting to $url")
-    resp = HTTP.post(url, headers, body)
+    resp = HTTP.post(url, [], body)
     println("got response ", resp)
 end
 
